d2980e012b015d37a57a23c212307adb891a1fa7
Manuel Rebollo <mrebollob@gmail.com>
2018-12-18 19:20:43 +0100

    Add fabric config

--- app/src/main/java/com/mrebollob/leitnerbox/LeitnerBoxApplication.kt
+++ app/src/main/java/com/mrebollob/leitnerbox/LeitnerBoxApplication.kt
@@ -1,7 +1,11 @@
 package com.mrebollob.leitnerbox
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.answers.Answers
+import com.crashlytics.android.core.CrashlyticsCore
 import com.mrebollob.leitnerbox.di.mainModule
+import io.fabric.sdk.android.Fabric
 import io.github.inflationx.calligraphy3.CalligraphyConfig
 import io.github.inflationx.calligraphy3.CalligraphyInterceptor
 import io.github.inflationx.viewpump.ViewPump
@@ -16,10 +20,33 @@ class LeitnerBoxApplication : Application() {
 
         startKoin(this, listOf(mainModule))
         iniCalligraphy()
+        initializeCrashlytics()
 
         if (BuildConfig.DEBUG) {
             Timber.plant(DebugTree())
         }
+    }
+
+    private fun initializeCrashlytics() {
+        val crashlyticsCore = CrashlyticsCore.Builder()
+            .disabled("debug" == BuildConfig.BUILD_TYPE)
+            .build()
+
+        val crashlytics = Crashlytics.Builder()
+            .core(crashlyticsCore)
+            .build()
+
+        val answers = Answers()
+
+        val fabric = Fabric.Builder(applicationContext)
+            .kits(crashlytics, answers)
+            .debuggable(BuildConfig.DEBUG)
+            .build()
+
+        Fabric.with(fabric)
+
+        Crashlytics.setString("GIT_SHA_KEY", BuildConfig.GIT_SHA)
+        Crashlytics.setString("BUILD_TIME", BuildConfig.BUILD_TIME)
     }
 
     private fun iniCalligraphy() {
